ff864268dc6e243457ecc35a9b24cf19f5f1bfe0
IanKe2006 <ianke2006@gmail.com>
2026-09-20 21:25:47 +0800

    homework10

---
+++ homework10.kt
@@ -0,0 +1,16 @@
+fun main() {
+    println("1+2等於" + (1 + 2))
+    println("3*4等於" + (3 * 4))
+
+    var num1 = 2
+    var num2 = 3
+    val sum = num1 + num2
+
+    println("變數num1的值是" + num1)
+    println("變數num2的值是" + num2)
+    println("num1+num2的值是" + sum)
+
+    num1 = num1 + 1
+
+    println("變數num1的值加1之後是" + num1)
+}
