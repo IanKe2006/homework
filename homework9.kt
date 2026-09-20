2f1ee607830bea5324db873064b91cf14aec5467
IanKe2006 <ianke2006@gmail.com>
2026-09-20 21:21:53 +0800

    homework9

---
+++ homework9.kt
@@ -0,0 +1,9 @@
+fun main() {
+    println("請輸入一個整數")
+    val str1 = readLine()         
+    val num = str1!!.toInt()       
+    println("您輸入的數字是:$num")
+    println("請輸入字串")
+    val str2 = readLine()
+    println("剛剛輸入的字串是:$str2")
+}
