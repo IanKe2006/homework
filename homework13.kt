62481dee9dc305a8d0e2ee3986c160974ef25d99
IanKe2006 <ianke2006@gmail.com>
2026-09-20 22:04:15 +0800

    homework13

---
+++ homework13.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val d = 2
+    val pi = 3.14
+    println("直徑是" + d + "公分的圓")
+    println("其圓周為" + (d * pi) + "公分")   
+    val num1 = 5
+    val num2 = 4
+    val div1 = num1 / num2                      
+    val div2 = num1.toDouble() / num2.toDouble()   
+    println("5/4等於" + div1)
+    println("5/4等於" + div2)
+}
