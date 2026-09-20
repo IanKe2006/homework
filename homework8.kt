38bb48c6a5898cde303f10ab37f5bb2d3e62efd5
IanKe2006 <ianke2006@gmail.com>
2026-09-20 21:07:49 +0800

    homework8

---
+++ homework8.kt
@@ -0,0 +1,9 @@
+fun main() {
+    var num1: Int    
+    var num2: Int
+    num1 = 3    
+    println("變數num1的值是:" + num1)
+    num2 = num1    
+    println("將變數num1指定到變數num2之中")
+    println("變數num2的值是:" + num2)
+}
